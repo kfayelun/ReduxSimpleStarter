b4465be29d21e432cd84f4f57742d01170a687df
Kristi Faye-Lund <Kristi.Faye-Lund@derivco.co.za>
2018-01-17 09:21:28 +0100

    Section 5 of course finished

---
+++ src/containers/search_bar.js
@@ -0,0 +1,51 @@
+/**
+ * Created by kristiNewMac on 2018/01/15.
+ */
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''};
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  // Get weater data
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Get a 5 day forecast of your favourite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    )
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,12 @@
+/**
+ * Created by kristiNewMac on 2018/01/15.
+ */
+import { FETCH_WEATHER } from '../actions/index';
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // return state.concat([ action.payload.data ]);
+      return [ action.payload.data, ...state ];
+  }
+  return state;
+}
